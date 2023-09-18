495bce9f711932c4bfa9e0cb90448038fd93c834
Jian Jeraus Young <jianjerausyoung37@gmail.com>
2023-09-18 18:53:39 +0700

    Rveisi JS Room Dinamis

--- JS/room.js
+++ JS/room.js
@@ -1,15 +1,25 @@
 const modalactive = document.getElementById("modal-active");
 
 function openDialog(clickedDiv) {
+    let roomNum = clickedDiv.firstElementChild.innerHTML;
     var classNames = clickedDiv.className.split(" ");
     for (var i = 0; i < classNames.length; i++) {
       var className = classNames[i];
       if (className !== "room-frame") {
-        document.getElementById(className).style.display = 'block';
+        document.querySelector("dialog > .Hos-Name").nodeValue = roomNum;
+        document.getElementById(className).showModal();
+        
       }
     }
   }
 
   function closeDialog(dialogId) {
-    document.getElementById(dialogId).style.display = 'none';
+    document.getElementById(dialogId).close();
+  }
+
+  const rooms = document.querySelectorAll ('.room-frame');
+  for (let index = 0; index < rooms.length; index++) {
+    const room = rooms[index].firstElementChild;
+    let temp = index+501
+    room.innerHTML = "R. " + temp;
   }
