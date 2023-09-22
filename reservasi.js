4838c21e7ddeb6982147e2b4810eff64103506d7
Shanti Putri Syawalina <126060834+shanshane17@users.noreply.github.com>
2023-09-22 11:51:12 +0700

    reservasi.js

--- reservasi.js
+++ reservasi.js
@@ -10,8 +10,9 @@ function submitForm() {
     };
   
     if (isFormFilled(formData)) {
-      alert('Your Reservation Successfully!');
       sendData(formData);
+      alert('Your Reservation Successfully!');
+      window.location.assign("clickRoom.html");
     } else {
       alert('Your reservation has failed, please fill in the data correctly!');
     }
@@ -33,35 +34,8 @@ function sendData(formData) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData)
-    //}).then(response => {
-        //if (!response.ok) {
-          //throw new Error('Network response was not ok');
-        //}
-        //return response.json();
     }).then(data => {
         console.log('Response:', data);
         alert('Data anda berhasil dikirim!');
     }).catch(error => console.error('Error:', error));
 }
-  
-
-
-//const form = document.querySelector('datapasien');
-//const name = document.querySelector('#name');
-//const alamat = document.querySelector('#alamat');
-//const tanggal_lahir = document.querySelector('#tanggal_lahir');
-//const tempat_lahir = document.querySelector('#tempat_lahir');
-//const no_hp = document.querySelector('#no_hp');
-//const room_number = document.querySelector('#room_number');
-//const room_type = document.querySelector('#room_type');
-
-// test 
-//form.addEventListener('submit', ()=> {
-   //console.log(name.value);
-   //console.log(alamat.value);
-   //console.log(tanggal_lahir.value);
-   //console.log(tempat_lahir.value);
-   //console.log(no_hp.value);
-   //console.log(room_number.value);
-   //console.log(room_type.value);
-//});
